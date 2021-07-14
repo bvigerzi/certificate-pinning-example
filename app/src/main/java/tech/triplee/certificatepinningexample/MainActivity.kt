cb3a43fcff8534e2f3fe1da66aa35cccefb63cf4
bvigerzi <48936239+bvigerzi@users.noreply.github.com>
2021-07-14 21:17:01 +1000

    Add certificate pinner code

--- app/src/main/java/tech/triplee/certificatepinningexample/MainActivity.kt
+++ app/src/main/java/tech/triplee/certificatepinningexample/MainActivity.kt
@@ -2,10 +2,32 @@ package tech.triplee.certificatepinningexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val certificatePinner = CertificatePinner.Builder().add("triplee.tech", "sha256/AAAA").build()
+        val client = OkHttpClient.Builder().certificatePinner(certificatePinner).build()
+        client.newCall(Request.Builder().url("https://triplee.tech").build()).enqueue(object :
+            Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    for ((name, value) in response.headers) {
+                        println("$name: $value")
+                    }
+
+                    println(response.body!!.string())
+                }
+            }
+        })
         setContentView(R.layout.activity_main)
     }
 }
