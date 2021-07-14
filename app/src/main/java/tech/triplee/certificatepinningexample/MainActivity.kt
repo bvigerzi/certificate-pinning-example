e7a34af4d4fecf6bf40d74ba7b5ab5e2197d2e38
bvigerzi <48936239+bvigerzi@users.noreply.github.com>
2021-07-14 21:14:05 +1000

    Initial template

---
+++ app/src/main/java/tech/triplee/certificatepinningexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package tech.triplee.certificatepinningexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
